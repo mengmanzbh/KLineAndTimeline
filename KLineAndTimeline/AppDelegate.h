fb9385c26bed83c4e4796c9823bb7135fe8663b1
mengmanzbh <1361006944@qq.com>
2017-02-16 17:06:27 +0800

    KO
    OK

---
+++ KLineAndTimeline/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KLineAndTimeline
+//
+//  Created by 谢霆锋 on 2017/2/16.
+//  Copyright © 2017年 zbh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KLineAndTimeline/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KLineAndTimeline
+//
+//  Created by 谢霆锋 on 2017/2/16.
+//  Copyright © 2017年 zbh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
